01974198ec58fa1e658e1c498bf72b981b079ddc
Biplav Poudel <biplavpoudel764@gmail.com>
2024-04-25 11:02:47 +0545

    Name of movies successfully extracted

--- app/browser.py
+++ app/browser.py
@@ -1,6 +1,7 @@
 from RPA.Browser.Selenium import Selenium
 from selenium.webdriver.chrome.options import Options
-import unittest
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 # sel = Selenium()
 
@@ -10,10 +11,9 @@ class Browser():
         self.url, self.headless, self.maximize = browser_lib
         try:
             options = Options()
-            # options.add_argument("--no-sandbox")
-            # options.add_argument("--disable-gpu")
-            # self.sel.open_available_browser(url = self.url, headless=self.headless, browser_selection='chrome', maximized = self.maximize, options = options)
-            self.sel.open_chrome_browser(url=self.url)
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            self.sel.open_available_browser(url = self.url, headless=self.headless, maximized = self.maximize, options = options)
         except Exception as e:
             print(f"Error captured as: {str(e)}")
             self.sel.driver.quit()
@@ -24,9 +24,15 @@ class Browser():
     def search(self, movie) -> None:
         self.sel.go_to(self.url+ f"search/movie?query={movie}")
         self.sel.wait_until_element_is_visible('//*[@id="main"]/section/div/div/div[2]')
-        count = self.sel.get_element_count('//*[@id="main"]/section/div/div/div[2]/section/div[1]/div/div') - 1
-        print(f"The number of elements in {movie} page is {count}")
-        
+        count = self.sel.get_element_count('//*[@id="main"]/section/div/div/div[2]/section/div[1]/div/div')
+        print(f"\nThe number of elements in {movie} page is {count-1}")
+        #count counts extra nav-page div element
+
+        #Now comes the comparision part
+        for i in range(1, count):
+            movie_name = self.sel.get_text(f'//*[@id="main"]/section/div/div/div[2]/section/div[1]/div/div[{i}]/div/div[2]/div[1]/div/div/a/h2')
+            print(movie_name)
+
 
 
     def close_browser(self) -> None:
